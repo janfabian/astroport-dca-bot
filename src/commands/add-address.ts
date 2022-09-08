d9aed1d49d30791978fe914fd99ee78cce7f80df
janfabian <mail@janfabian.cz>
2022-09-08 16:47:24 +0200

    add-address

--- src/commands/add-address.ts
+++ src/commands/add-address.ts
@@ -2,12 +2,17 @@ import { Command } from 'commander'
 import { write } from '../db.js'
 import { tryCatch } from '../lib.js'
 import getUserDcaOrders from '../queries/get-user-dca-orders.js'
-import { getLCDClient } from '../terra.js'
+import { getKey, getLCDClient } from '../terra.js'
 import { DcaQueryInfo } from '../types/astroport.js'
 import { User } from '../types/db.js'
 
 export async function addAddress(address, options) {
+  const k = getKey()
   const terra = await getLCDClient()
+  const wallet = terra.wallet(k)
+
+  address = address ?? wallet.key.accAddress
+
   const result: DcaQueryInfo[] = await terra.wasm.contractQuery(
     process.env.ASTROPORT_DCA as string,
     getUserDcaOrders(address),
@@ -33,7 +38,7 @@ export default function addAddressCommand(program: Command) {
     .description(
       'Add address to user DB, watch all of users orders unless order ids explicitly provided',
     )
-    .argument('<string>', 'address')
+    .argument('[string]', 'address')
     .option('-o, --order-ids <numbers...>', 'order ids')
     .action(tryCatch(addAddress))
 }
