649387e1f3e66349fe9044311e8004f4372afd86
janfabian <mail@janfabian.cz>
2022-09-15 12:49:43 +0200

    add bot tip command test

---
+++ src/commands/add-bot-tip.test.ts
@@ -0,0 +1,121 @@
+import { getMockedLCDClient } from './test-lib.js'
+import * as terra from '../terra.js'
+import * as astroport from '../astroport.js'
+import * as addBotTipExecute from '../executes/add-bot-tip.js'
+import { addBotTip } from './add-bot-tip.js'
+import { InvalidArgumentError } from 'commander'
+
+jest.mock('../terra.js')
+jest.mock('../astroport.js')
+jest.mock('../executes/add-bot-tip.js')
+
+const mockedTerra = jest.mocked(terra, true)
+const mockedAstroport = jest.mocked(astroport, true)
+const mockedAddBotTipExecute = jest.mocked(addBotTipExecute, true)
+
+describe('addBotTip', () => {
+  it('adds funds for bot tip fees', async () => {
+    const client = getMockedLCDClient()
+
+    mockedTerra.getLCDClient.mockResolvedValue(client)
+    mockedAstroport.getPairs.mockResolvedValue([
+      {
+        asset_infos: [
+          {
+            native_token: {
+              denom: 'uluna',
+            },
+          },
+          {
+            token: {
+              contract_addr: 'token_contract_addr',
+            },
+          },
+        ],
+        contract_addr: 'contract_addr',
+        liquidity_token: 'liquidity_token',
+        pair_type: {},
+      },
+    ])
+
+    await addBotTip({ assets: ['1000', 'uluna'] })
+
+    expect(mockedTerra.getKey).toBeCalledTimes(1)
+    expect(mockedAddBotTipExecute.default).toBeCalledTimes(1)
+    expect(mockedAddBotTipExecute.default).toBeCalledWith(expect.any(Object), [
+      {
+        amount: '1000',
+        info: {
+          native_token: {
+            denom: 'uluna',
+          },
+        },
+      },
+    ])
+  })
+
+  it('adds multiple assets', async () => {
+    const client = getMockedLCDClient()
+
+    mockedTerra.getLCDClient.mockResolvedValue(client)
+    mockedAstroport.getPairs.mockResolvedValue([
+      {
+        asset_infos: [
+          {
+            native_token: {
+              denom: 'uluna',
+            },
+          },
+          {
+            token: {
+              contract_addr: 'token_contract_addr',
+            },
+          },
+        ],
+        contract_addr: 'contract_addr',
+        liquidity_token: 'liquidity_token',
+        pair_type: {},
+      },
+    ])
+
+    await addBotTip({
+      assets: ['1000', 'uluna', '100', 'random_token_contract_addr'],
+    })
+
+    expect(mockedTerra.getKey).toBeCalledTimes(1)
+    expect(mockedAddBotTipExecute.default).toBeCalledTimes(1)
+    expect(mockedAddBotTipExecute.default).toBeCalledWith(expect.any(Object), [
+      {
+        amount: '1000',
+        info: {
+          native_token: {
+            denom: 'uluna',
+          },
+        },
+      },
+      {
+        amount: '100',
+        info: {
+          token: {
+            contract_addr: 'random_token_contract_addr',
+          },
+        },
+      },
+    ])
+  })
+
+  it('fails if wrong number of parameters', async () => {
+    const client = getMockedLCDClient()
+
+    mockedTerra.getLCDClient.mockResolvedValue(client)
+
+    expect.assertions(1)
+    try {
+      await addBotTip({
+        assets: ['1000'],
+      })
+    } catch (e) {
+      expect(e).toBeInstanceOf(InvalidArgumentError)
+    }
+  })
+})
